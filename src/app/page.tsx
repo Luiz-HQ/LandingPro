662aa7e4af321c2eb8a833e0ca2d4ffb8ab64b6c
LuizDev <luizhn1703@gmail.com>
2025-09-19 16:28:33 -0300

    I created header and first impression of landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,102 @@
-import Image from "next/image";
+import {
+  ArrowRight,
+  CheckCircle,
+  Clock,
+  DollarSign,
+  Globe,
+  MessageCircle,
+  Zap,
+} from "lucide-react";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <main className="bg-gradient-to-br from-slate-50 to-blue-50 ">
+      <header className="bg-white/80 backdrop-blur-md border-b border-gray-200 sticky top-0 z-50 ">
+        <div className="container mx-auto p-4 flex items-center justify-between">
+          <div className="flex items-center gap-2">
+            <Globe className="h-8 w-8 text-blue-600" />
+            <span className="text-xl font-bold text-gray-800">LandingPro</span>
+          </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+          <nav className="hidden gap-8 md:flex items-center">
+            <a
+              href="#services"
+              className="text-gray-600 hover:text-blue-600 transition-colors"
+            >
+              Serviços
+            </a>
+            <a
+              href="#benefits"
+              className="text-gray-600 hover:text-blue-600 transition-colors"
+            >
+              Benefícios
+            </a>
+            <a
+              href="#depoiments"
+              className="text-gray-600 hover:text-blue-600 transition-colors"
+            >
+              Depoimentos
+            </a>
+            <a
+              href="#contact"
+              className="text-gray-600 hover:text-blue-600 transition-colors"
+            >
+              Contato
+            </a>
+          </nav>
+
+          <button className="bg-[#171717] hover:bg-[#222222] px-4 py-2 rounded-md text-[#FDFEFF] font-bold cursor-pointer text-[14px] hidden md:flex items-center gap-2">
+            <MessageCircle className="w-4" />
+            Falar Agora
+          </button>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      </header>
+
+      <section>
+        <div className="py-20 px-4">
+          <div className="container mx-auto text-center">
+            <div className="flex items-center justify-center gap-2 mb-4">
+              <p className="bg-blue-100 text-[10px] text-[#2445c9] font-semibold w-[110px] rounded-lg p-1 mb-6 hover:bg-blue-200">
+                ⚡ Entrega Rapida!
+              </p>
+            </div>
+
+            <h1 className="text-gray-900 flex flex-col text-4xl md:text-6xl leading-tight font-bold mb-6">
+              Landing Pages que
+              <span className="text-blue-600">Convertem Visitantes</span> em
+              Clientes
+            </h1>
+            <p className="text-xl text-gray-600 mb-8 max-w-3xl mx-auto">
+              Crie uma presença online profissional para seu negócio com landing
+              pages modernas, responsivas e otimizadas para conversão. Entrega
+              rápida e preço justo!
+            </p>
+
+            <div className="flex items-center justify-center gap-2">
+              <button className="bg-[#155DFC] hover:bg-[#003ab8] flex items-center justify-center gap-2 cursor-pointer rounded-md text-[#FDFEFF] p-3">
+                <Zap className="h-5 w-5 mr-2" />
+                Quero um Orçamento GRÁTIS!
+                <ArrowRight className="h-5 w-5 ml-2" />
+              </button>
+            </div>
+
+            <div className="mt-12 flex flex-wrap justify-center items-center gap-8 text-sm text-gray-500">
+              <div className="flex items-center">
+                <Clock className="h-4 w-4 mr-2 text-green-600" />
+                Entrega em 48h
+              </div>
+              <div className="flex items-center">
+                <DollarSign className="h-4 w-4 mr-2 text-green-600" />
+                Preço justo
+              </div>
+              <div className="flex items-center">
+                <CheckCircle className="h-4 w-4 mr-2 text-green-600" />
+                Suporte incluído
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
   );
 }
